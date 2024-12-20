5a80a9f378cacce5cf5eb32018fea14a75fb0794
CiuraD <127660370+CiuraD@users.noreply.github.com>
2024-12-20 13:37:47 +0100

    Add auto-generated ID for ActivationCode and User entities

--- quiz_backend/src/main/java/com/quiz/quiz_backend/model/ActivationCode.java
+++ quiz_backend/src/main/java/com/quiz/quiz_backend/model/ActivationCode.java
@@ -2,6 +2,8 @@ package com.quiz.quiz_backend.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -12,6 +14,7 @@ import jakarta.persistence.Table;
 public class ActivationCode {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_kodu")
     private int codeId;
 
--- quiz_backend/src/main/java/com/quiz/quiz_backend/model/User.java
+++ quiz_backend/src/main/java/com/quiz/quiz_backend/model/User.java
@@ -5,6 +5,8 @@ import java.util.Set;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
@@ -16,6 +18,7 @@ import jakarta.persistence.Table;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_uzytkownika")
     private int userId;
 
